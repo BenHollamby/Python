ef78d04d5d03f471e5f36f57bec82494196d7997
emusteric <ben.hollamby@outlook.com>
2021-03-30 14:22:58 +1300

    finished chapter 9

--- Chapter 9 Classes.py
+++ Chapter 9 Classes.py
@@ -157,4 +157,102 @@ calling_name.describes_user()
 calling_name.reset_login_attempts()
 calling_name.describes_user()
 
-#continue from page 235
+#Ice Cream Stand: An ice cream stand is a specific kind of restaurant. Write a class called
+#IceCreamStand that inherits from the Restaurant class you wrote in Exercise 9-1 (page 162) or
+#Exercise 9-4 (page 167). Either version of the class will work; just pick the one you like better.
+#Add an attribute called flavors that stores a list of ice cream flavors. Write a method thatdisplays these flavors. Create an instance of IceCreamStand, and call this method.
+class Icecreamstand(Restaurant):
+    
+    def __init__(self, restaurant_name, cuisine_type):
+        super().__init__(restaurant_name, cuisine_type)
+
+    def flavors(self):
+        flavor_list = ['strawberry', 'trumpet', 'gum drops']
+        print("Our current selection of flavours are:")
+        for flavor in flavor_list:
+            print(f"\t{flavor}")
+
+
+creamy = Icecreamstand('bens', 'ice cream')
+
+creamy.describe_restaurant()
+creamy.is_open()
+creamy.flavors()
+
+#Admin: An administrator is a special kind of user. Write a class called Admin that inherits
+#from the User class you wrote in Exercise 9-3 (page 162) or Exercise 9-5 (page 167). Add an
+#attribute, privileges, that stores a list of strings like "can add post", "can delete post",
+#"can ban user", and so on. Write a method called show_privileges() that lists the
+#administrator’s set of privileges. Create an instance of Admin, and call your method.
+class Admin(User):
+    
+    def __init__(self, first_name, last_name, age, occupation):
+        super().__init__(first_name, last_name, age, occupation)
+        self.privileges = ['can add post', 'can delete post', 'can ban user']
+    
+    def show_privileges(self):
+        permissions = self.privileges
+        print(f"{self.first_name.title()} {self.last_name.title()} has the following permissions") 
+        for p in permissions:
+            print(f"\t{p}")
+
+ituser = Admin('ben', 'holla', 27, 'information technology')
+
+ituser.show_privileges()
+
+#Dice: Make a class Die with one attribute called sides, which has a default value of 6.
+#Write a method called roll_die() that prints a random number between 1 and the number of
+#sides the die has. Make a 6-sided die and roll it 10 times.
+#Make a 10-sided die and a 20-sided die. Roll each die 10 times.
+from random import randint
+
+class Die:
+
+    def __init__(self, sides):
+        self.sides = sides
+    
+    def roll_die(self):
+        print(randint(1,self.sides))
+
+tyeee = Die(6)
+tyeee.roll_die()
+tyeee.roll_die()
+
+tyeeeee = Die(10)
+tyeeeee.roll_die()
+tyeeeee.roll_die()
+
+tyeeee = Die(20)
+tyeeee.roll_die()
+tyeeee.roll_die()
+
+#Lottery: Make a list or tuple containing a series of 10 numbers and five letters.
+#Randomly select four numbers or letters from the list and print a message saying that any ticket
+#matching these four numbers or letters wins a prize.
+from random import choices
+
+lottery_list = ['a', 'b', 'c', 'd', 'e', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+test = choices(lottery_list, k=4)
+print(test)
+
+#Lottery Analysis: You can use a loop to see how hard it might be to win the kind of
+#lottery you just modeled. Make a list or tuple called my_ticket. Write a loop that keeps pulling
+#numbers until your ticket wins. Print a message reporting how many times the loop had to run
+#to give you a winning ticket.
+
+from random import choices
+
+lottery_list = ['a', 'b', 'c', 'd', 'e', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #easy to change this to nz lotto with range(1,41)
+
+my_pick = [10, 'a', 5, 'd'] #and then pick 6 digits, could probably work out a powerball one as well. Not that it would help.
+
+number_of_times = 0
+
+while True:
+    test = choices(lottery_list, k=4)
+    if test == my_pick:
+        print(f"Congrats, your numbers were rolled after {number_of_times}!")
+        break
+    else:
+        number_of_times += 1
