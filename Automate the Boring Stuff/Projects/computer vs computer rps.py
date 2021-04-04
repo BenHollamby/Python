15309c9bb82274203081215e40b9d570e476d95b
emusteric <ben.hollamby@outlook.com>
2021-04-04 16:25:48 +1200

    formatting, needs documentation, very happy

--- Automate the Boring Stuff/Projects/computer vs computer rps.py
+++ Automate the Boring Stuff/Projects/computer vs computer rps.py
@@ -9,48 +9,62 @@ rock_paper_scissor = ['r', 'p', 's']
 
 computer1_wins = 0
 computer1_losses = 0
+
 ties = 0
+
 computer2_wins = 0
 computer2_losses = 0
 
 number_of_games = 0
 
 while number_of_games != 100000000:
+
     computer_choice1 = random.choice(rock_paper_scissor)
     computer_choice2 = random.choice(rock_paper_scissor)
     number_of_games += 1
 
     if computer_choice1 == computer_choice2:
         ties += 1
+
     elif computer_choice1 == 'r' and computer_choice2 == 's':
         computer1_wins += 1
         computer2_losses += 1
+
     elif computer_choice1 == 's' and computer_choice2 == 'r':
         computer1_losses += 1
         computer2_wins += 1
+
     elif computer_choice1 == 'p' and computer_choice2 == 'r':
         computer1_wins += 1
         computer2_losses += 1
+
     elif computer_choice1 == 'p' and computer_choice2 == 's':
         computer1_losses += 1
         computer2_wins += 1
+
     elif computer_choice1 == 's' and computer_choice2 == 'p':
         computer1_wins += 1
         computer2_losses += 1
+
     elif computer_choice1 == 'r' and computer_choice2 == 'p':
         computer1_losses += 1
         computer2_wins += 1
 
 print(f"There were {number_of_games} games played!")
+
 print(f"{ties} of those were tied games!")
+
 print(f"Computer 1 won {computer1_wins} games,and lost {computer1_losses} games!")
+
 print(f"Computer 2 won {computer2_wins} games and lost {computer2_losses} games!")
 
 if computer1_wins == computer2_wins:
     print(f"The result is a tie after {number_of_games}!")
+
 elif computer1_wins > computer2_wins:
     comp1_by = (computer1_wins - computer2_wins)
     print(f"Computer 1 is the overall winner with {computer1_wins} to {computer2_wins} winning by {comp1_by} match points!")
+    
 else:
     comp2_by = (computer2_wins - computer1_wins)
     print(f"Computer 2 is the winner with {computer2_wins} to {computer1_wins} winning by {comp2_by} match points!")
